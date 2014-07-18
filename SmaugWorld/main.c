897bb4625dfbc5f49f430de2c0905b565c35c604
csvenja <c.svenjax@gmail.com>
2014-07-17 23:21:58 -0700

    Initial Commit

---
+++ SmaugWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SmaugWorld
+//
+//  Created by Svenja on 2014-07-17.
+//  Copyright (c) 2014 Shan Cao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
